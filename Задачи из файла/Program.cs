7832d437f5107854d669111f56ee843a74cec89f
79046 <79046@LAPTOP-279GGFSI>
2024-11-08 02:10:33 +0300

    Добавьте файлы проекта.

---
+++ Задачи из файла/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задачи_из_файла
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            /* 1) Вывести на экран число e(основание натурального логарифма) с точностью до десятых */
+            Console.WriteLine("номер 1");
+            Console.WriteLine("e = 2.7");
+
+            /* 2) Вывести на экран числа 50 и 10 одно под другим */
+            Console.WriteLine("номер 2");
+            Console.WriteLine(50);
+            Console.WriteLine(10);
+            Console.ReadKey();
+
+            /* 3) Составить программу вывода на экран «столбиком» четырех любых чисел */
+            Console.WriteLine("номер 3");
+            Console.WriteLine(""
+        }
+    }
+}
---
+++ ЗадачиТумакова/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЗадачиТумакова
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1) Написать программу, которая спрашивает имя
+            пользователя, и затем приветствует пользователя по имени*/
+            Console.WriteLine("Упражнение 2.1");
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name);
+
+            /* 2)Написать программу, которой на вход подается два
+            целых числа, на выходе – результат деления одного числа на другое.
+            Предусмотреть обработку исключительной ситуации, возникающей при
+            делении числа на ноль */
+            Console.WriteLine("Упражнение 2.2");
+            Console.WriteLine("Введите два любых целых числа:");
+            float x1 = Convert.ToInt32(Console.ReadLine());
+            float x2 = Convert.ToInt32(Console.ReadLine());
+            if (x2 == 0)
+            { Console.WriteLine("Действие невозможно"); }
+            else { Console.WriteLine("Результат деления:" + (x1  / x2)); }
+
+            /* 3) Прочитать букву с экрана и вывести на печать
+            следующую за ней букву в алфавитном порядке */
+            Console.WriteLine("Домашнее задание 2.1");
+            Console.WriteLine("Введите любую прописную букву алфавита:");
+            string alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяа";
+            int letter = alfavit.IndexOf(Console.ReadLine());
+            Console.WriteLine(alfavit[letter + 1]);
+
+            /* 4) Написать программу, которая решает квадратное
+            уравнение. Входные данные – коэффициенты уравнения, выходные –
+            найденные корни */
+            Console.WriteLine("Домашнее задание 2.2");
+            Console.WriteLine("Введите коэффиценты уравнение:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int discriminant = b * b - 4 * a * c;
+            if (discriminant >= 0)
+            {
+                double z1 = (-1 * b + Math.Sqrt(discriminant)) / (2 * a);
+                double z2 = (-1 * b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine($"x1 = {z1}, x2 = {z2}");
+            }
+            else { Console.WriteLine("Нет корней"); }
+            Console.ReadKey();  
+        }
+    }
+}
